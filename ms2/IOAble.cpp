e55d4d4f7f56fef9d0d1d03f06eebaa51db50398
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2020-11-13 21:36:03 -0500

    Milestone 2 released

---
+++ ms2/IOAble.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "IOAble.h"
+using namespace std;
+namespace sdds {
+   
+}
---
+++ ms2/IOAble.h
@@ -0,0 +1,10 @@
+#ifndef SDDS_IOABLE_H_
+#define SDDS_IOABLE_H_
+#include <iostream>
+namespace sdds {
+   class IOAble {
+   public:
+
+   };
+}
+#endif // !SDDS_IOABLE_H_
